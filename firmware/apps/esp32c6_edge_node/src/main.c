9ff14d895704cf160cd4c54c129516f8c0df3f4f
Grzegorz Grzęda <grzegorz.grzeda@g2labs.pl>
2026-02-17 01:18:41 +0100

    Create images signed for mcuboot

--- firmware/apps/esp32c6_edge_node/src/main.c
+++ firmware/apps/esp32c6_edge_node/src/main.c
@@ -1,16 +1,46 @@
 #include <zephyr/app_version.h>
+#include <zephyr/autoconf.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
+#include <zephyr/net/conn_mgr_monitor.h>
 #include <zephyr/net/net_event.h>
 #include <zephyr/net/net_if.h>
 #include <zephyr/net/net_mgmt.h>
 #include <zephyr/net/wifi_mgmt.h>
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
+#define WIFI_TIMER_TIMEOUT_SECONDS 10
+
+static void wifi_timer_expiry_cb(struct k_timer* timer);
 
 K_SEM_DEFINE(wifi_sem, 0, 1);
 K_MUTEX_DEFINE(wifi_mutex);
+K_TIMER_DEFINE(wifi_timer, wifi_timer_expiry_cb, NULL);
 static bool wifi_connected = false;
+
+static int wifi_connect_stored() {
+    struct net_if* iface = net_if_get_wifi_sta();
+    if (iface == NULL) {
+        LOG_ERR("Failed to get Wi-Fi interface");
+        return -1;
+    }
+
+    k_timer_start(&wifi_timer, K_SECONDS(WIFI_TIMER_TIMEOUT_SECONDS),
+                  K_NO_WAIT);
+
+    int rc = net_mgmt(NET_REQUEST_WIFI_CONNECT_STORED, iface, NULL, 0);
+    if (rc) {
+        LOG_ERR("Failed to connect to Wi-Fi: %d", rc);
+        return -1;
+    }
+    return 0;
+}
+
+static void wifi_timer_expiry_cb(struct k_timer* timer) {
+    ARG_UNUSED(timer);
+    LOG_ERR("Wi-Fi connection timed out");
+    wifi_connect_stored();
+}
 
 static void net_mgmt_event_handler(struct net_mgmt_event_callback* cb,
                                    uint64_t mgmt_event,
@@ -20,11 +50,13 @@ static void net_mgmt_event_handler(struct net_mgmt_event_callback* cb,
 
     if (mgmt_event == NET_EVENT_L4_CONNECTED) {
         LOG_INF("Network connected");
+        k_timer_stop(&wifi_timer);
         k_mutex_lock(&wifi_mutex, K_FOREVER);
         wifi_connected = true;
         k_mutex_unlock(&wifi_mutex);
         k_sem_give(&wifi_sem);
     } else if (mgmt_event == NET_EVENT_L4_DISCONNECTED) {
+        k_timer_stop(&wifi_timer);
         LOG_INF("Network disconnected");
         k_mutex_lock(&wifi_mutex, K_FOREVER);
         wifi_connected = false;
@@ -58,13 +90,9 @@ static int wifi_init(void) {
         &wifi_cb, net_mgmt_event_handler,
         NET_EVENT_L4_CONNECTED | NET_EVENT_L4_DISCONNECTED);
     net_mgmt_add_event_callback(&wifi_cb);
+    conn_mgr_mon_resend_status();
 
-    int rc = net_mgmt(NET_REQUEST_WIFI_CONNECT_STORED, iface, NULL, 0);
-    if (rc) {
-        LOG_ERR("Failed to connect to Wi-Fi: %d", rc);
-        return -1;
-    }
-    return 0;
+    return wifi_connect_stored();
 }
 
 int main(void) {
